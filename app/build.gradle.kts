20fe42fb5f626417e1f3c64c6bdf054c6cf0c7d5
Dr-Fate <arqlpereira@gmail.com>
2025-06-11 22:50:53 -0300

    Auto-scroll al agregar una nueva medición por kilómetro , arregla dependencias para el release + arreglo menor en UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.regularitytracker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 34
-        versionName = "2.3.0"
+        versionCode = 35
+        versionName = "2.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
